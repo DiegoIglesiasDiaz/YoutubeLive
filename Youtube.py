3dba2295ca314b955e959d5201e09341f161fe68
DiegoIglesiasDiaz <diegoiglesiasdiaz11@gmail.com>
2024-12-11 03:30:16 +0100

    update

--- Youtube.py
+++ Youtube.py
@@ -1,6 +1,6 @@
 import os
 
-dirname = os.getenv("VIDEO_DIRECTORY", "/var/app/")
+dirname = os.getenv("VIDEO_DIRECTORY", "//var/app/YoutubeLive/Videos")
 youtube_key = os.getenv("YOUTUBE_KEY", "0xx0-xxxx-x000-x0x0-xxxx")
 
 key = 'rtmp://a.rtmp.youtube.com/live2/'+ youtube_key 
@@ -9,5 +9,5 @@ while True:
     files = os.listdir(dirname)  
     for f in files: 
             if ".mp4" in f: 
-                    cmd = "ffmpeg -threads 3  -re -i " + dirname + f + " -c:v libx264 -preset ultrafast -crf 24 -g 3 -f flv " + key 
-                    os.system(cmd) 
+                cmd = f"ffmpeg -threads 3 -re -i {dirname}{f} -c:v libx264 -preset ultrafast -crf 24 -g 3 -f flv {key}"
+                os.system(cmd)
